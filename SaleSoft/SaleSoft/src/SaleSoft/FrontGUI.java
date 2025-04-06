5c573ad0de9eb08cb41514300f0477916a7eb7bf
elle17day <elle17day@outlook.com>
2025-04-06 12:59:46 +0100

    Base File Structure
    Uploading the starting file structure

---
+++ SaleSoft/SaleSoft/src/SaleSoft/FrontGUI.java
@@ -0,0 +1,12 @@
+package SaleSoft;
+
+import javax.swing.JFrame;
+
+public class FrontGUI extends JFrame{
+	private static final long serialVersionUID = 4725189688562458030L;
+
+	FrontGUI() {
+		
+	}
+
+}
---
+++ SaleSoft/SaleSoft/src/SaleSoft/Inventory.java
@@ -0,0 +1,5 @@
+package SaleSoft;
+
+public class Inventory {
+
+}
