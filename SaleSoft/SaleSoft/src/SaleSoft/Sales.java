3ef1f216ae4472dc072f8796fea32ba687e29d1a
Hui Chen <huic4278@gmail.com>
2025-04-07 04:23:45 +0100

    V1Sales.java

--- SaleSoft/SaleSoft/src/SaleSoft/Sales.java
+++ SaleSoft/SaleSoft/src/SaleSoft/Sales.java
@@ -2,27 +2,33 @@ package SaleSoft;
 
 public class Sales {
 private String name;
-	private int Stock;
-	private int LowStockwarning;
+	private String name;
 	private int Prise;
-
+	private int QtySold;
+	private int Totcost;
+	private int data;
+	private int time;
 	
 	void item() {
 		name = "";
 		Prise = 0;
-		Stock = 0;
-		LowStockwarning = 0;
-	}
-	
-	void item(String name, int prise, int Stock, int LowStockwarning) {
-		this.name = name;
-		this.Prise = prise;
-		this.Stock = Stock;
-		this.LowStockwarning = LowStockwarning;
+		QtySold = 0;
+		Totcost = 0;
+		data = 0;
+		time = 0;
 		
 	}
 	
+	void item(String name, int prise, int QtySold, int Totcost, int data, int time) {
+		this.name = name;
+		this.Prise = prise;
+		this.QtySold = QtySold;
+		this.Totcost = Totcost;
+		this.data = data;
+		this.time = time;
+	}
+	
 	public String toString() {
-		return "Name: " + name + "Prise: " + Prise + "Low Stock Warning" + LowStockwarning + "Stock" + Stock;	
+		return "Name: " + name + "Prise: " + Prise + "QtySold: " + QtySold + "Totcost: " + Totcost + "Data: " + data + "Time: " + time;	
 	}
 }
