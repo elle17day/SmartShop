fc1933a7f3a8644d19d293ecae4e5c69f24037ff
Hui Chen <huic4278@gmail.com>
2025-04-07 04:21:12 +0100

    V1Sales.java

--- SaleSoft/SaleSoft/src/SaleSoft/Sales.java
+++ SaleSoft/SaleSoft/src/SaleSoft/Sales.java
@@ -1,5 +1,28 @@
 package SaleSoft;
 
 public class Sales {
+private String name;
+	private int Stock;
+	private int LowStockwarning;
+	private int Prise;
 
+	
+	void item() {
+		name = "";
+		Prise = 0;
+		Stock = 0;
+		LowStockwarning = 0;
+	}
+	
+	void item(String name, int prise, int Stock, int LowStockwarning) {
+		this.name = name;
+		this.Prise = prise;
+		this.Stock = Stock;
+		this.LowStockwarning = LowStockwarning;
+		
+	}
+	
+	public String toString() {
+		return "Name: " + name + "Prise: " + Prise + "Low Stock Warning" + LowStockwarning + "Stock" + Stock;	
+	}
 }
