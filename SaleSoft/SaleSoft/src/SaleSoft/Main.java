0c1ff1d63967cb074a20cf034b72562d330955f6
RClague2025 <rjclague03@gmail.com>
2025-04-08 09:06:15 +0100

    Main.java
    - Added basic arraylist for storing data
    - Imported necessary java packages

--- SaleSoft/SaleSoft/src/SaleSoft/Main.java
+++ SaleSoft/SaleSoft/src/SaleSoft/Main.java
@@ -1,7 +1,14 @@
 package SaleSoft;
 
+import FrontGUI;
+import java.util.ArrayList;
+
 public class Main {
 
+	// Initalises the arraylist for data storing
+	ArrayList<Inventory> inventoryList = new ArrayList<Inventory>();
+	ArrayList<Sales> salesList = new ArrayList<Sales>();
+	
 	public static void main(String[] args) {
 		new FrontGUI();
 
