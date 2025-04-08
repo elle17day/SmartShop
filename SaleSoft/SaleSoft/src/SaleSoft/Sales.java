57c8537cce374a3f265e2d2cd7148c0afba94d9d
RClague2025 <rjclague03@gmail.com>
2025-04-08 08:57:59 +0100

    Sales.java
    - Updated variable names to follow standard naming conventions
    - Fixed spelling errors
    - Removed unnecessary method
    - Altered outputs to make them more user friendly

--- SaleSoft/SaleSoft/src/SaleSoft/Sales.java
+++ SaleSoft/SaleSoft/src/SaleSoft/Sales.java
@@ -1,34 +1,23 @@
 package SaleSoft;
 
 public class Sales {
-private String name;
 	private String name;
-	private int Prise;
-	private int QtySold;
-	private int Totcost;
+	private int price;
+	private int qtySold;
+	private int totCost;
 	private int data;
 	private int time;
 	
-	void item() {
-		name = "";
-		Prise = 0;
-		QtySold = 0;
-		Totcost = 0;
-		data = 0;
-		time = 0;
-		
-	}
-	
-	void item(String name, int prise, int QtySold, int Totcost, int data, int time) {
+	void item(String name, int price, int qtySold, int totCost, int data, int time) {
 		this.name = name;
-		this.Prise = prise;
-		this.QtySold = QtySold;
-		this.Totcost = Totcost;
+		this.price = prise;
+		this.qtySold = QtySold;
+		this.totCost = Totcost;
 		this.data = data;
 		this.time = time;
 	}
 	
 	public String toString() {
-		return "Name: " + name + "Prise: " + Prise + "QtySold: " + QtySold + "Totcost: " + Totcost + "Data: " + data + "Time: " + time;	
+		return "Name: " + name + "Price: " + Price + "Quantity Sold: " + qtySold + "Total Cost: " + totcost + "Data: " + data + "Time: " + time;	
 	}
 }
